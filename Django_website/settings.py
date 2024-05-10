09fd3f002f44d57c310c86f6ea82509c34b8d7f7
ramin77786408 <ramin77786408@gmail.com>
2024-05-10 11:00:22 +0000

    Initi postgresql

--- Django_website/settings.py
+++ Django_website/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Django_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'aaliam',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
